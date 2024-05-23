d47e08212bbb8343b73146935a09b971e9382b2e
mauricioCarcamo <mauriciorenecarcamorivera1@gmail.com>
2024-05-22 23:48:31 -0600

    fin

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrl: './lazy-image.component.css'
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+  ngOnInit(): void {
+    if ( !this.url ) 
+    throw new Error('Url property ie required');
+  }
+
+  @Input()
+  public url!: string
+  
+  @Input()
+  public alt: string = ""
+
+  public hasLoad: boolean = false
+
+  public onLoad(): void {
+    console.log("Loaded!!");
+    
+    this.hasLoad = true
+  }
 
 }
